72bd73691f04a3ea0fcb6cf615a2e132009bdac8
DJtheProgram <52799148+DJtheProgram@users.noreply.github.com>
2019-07-11 14:51:27 -0400

    Add files via upload

---
+++ lab7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void RotateFourChars(char* input);
+
+int main(void){
+	char alpha[4];
+   printf("Type 4 alphabet: \n");
+   scahf("%s", alpha);
+   RotateFourChars(&alpha);
+   return 0;
+}
+
+void RotateFourChars(char *input){
+	char temp;
+   int i,j;
+   for(i=0; i<4; i++){
+   printf("%s\n",input);
+   temp=input[0]
+   for(j=0;j<3;j++){
+   input[j]=input[j+1];
+   }
+   input[j]=temp;   
+	}
+}
